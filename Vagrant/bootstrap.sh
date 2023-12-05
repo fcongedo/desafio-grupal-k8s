b72f5dd99165baf8d3d51b5c98695140835199bc
fcongedo <franco_congedo@hotmail.com>
2023-12-05 13:51:39 -0300

    Instalacion de Helm agregada

--- Vagrant/bootstrap.sh
+++ Vagrant/bootstrap.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# update apt packages
+#update apt packages
 sudo apt-get update -y
 
-# Docker Install
+#Docker Install
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh ./get-docker.sh
 
-# Use Docker without sudo
+#Use Docker without sudo
 sudo groupadd docker
 sudo usermod -aG docker vagrant
 newgrp docker
@@ -19,3 +19,10 @@ sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 #Install minikube	
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+#Install Helm
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
